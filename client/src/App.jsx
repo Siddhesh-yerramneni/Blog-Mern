73b74e5140f52c8f9cb040d5305d093a813367e7
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2024-08-10 23:24:07 -0400

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 export default function App() {
   return (
-    <h1>Hi</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/projects' element={<Projects />}/>
+        <Route path='/signin' element={<SignIn />}/>
+        <Route path='/signup' element={<SignUp />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,10 @@
+// About.jsx
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      About
+    </div>
+  )
+}
