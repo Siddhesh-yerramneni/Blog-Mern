501daf3be317a644d39dc2778dd14b4889014263
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2024-08-12 10:51:46 -0400

    Created Signup api route

--- api/index.js
+++ api/index.js
@@ -2,14 +2,18 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
 const app = express();
 
+app.use(express.json());
+
 app.listen(3030, ()=> {
     console.log('Server is up and running on 3030');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
