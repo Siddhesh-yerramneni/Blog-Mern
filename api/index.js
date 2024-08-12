3ee8f0625ce29dabb3373262cd3924fe7554626c
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2024-08-12 10:08:07 -0400

    Created a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -10,3 +11,5 @@ const app = express();
 app.listen(3030, ()=> {
     console.log('Server is up and running on 3030');
 });
+
+app.use('/api/user', userRoutes);
