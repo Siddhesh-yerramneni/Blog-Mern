632520a5ef0b6b6800842637b68f90a60eae1566
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2024-08-17 18:13:27 -0400

    Completed Sidebar UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {useEffect, useState} from 'react';
+import {useLocation} from 'react-router-dom';
+import DashSideBar from '../Components/DashSideBar.jsx';
+import DashProfile from '../Components/DashProfile.jsx';
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab,setTab] = useState('');
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>
-      Dashboard
+  <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className='md: w-56'>
+      {/*sidebar */}
+      <DashSideBar />
     </div>
-  )
+    {/*profile */}
+    {tab==='profile' && <DashProfile/>}
+  </div>
+  );
 }
